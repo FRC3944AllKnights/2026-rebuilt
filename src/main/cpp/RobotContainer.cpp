44b4cc41bdfa27be7eb00af3233873bec31f36a7
TPARoboticsDesktop <TPARoboticsComp2@gmail.com>
2026-02-16 10:46:11 -0700

    Mid-debug

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -18,13 +18,11 @@ void RobotContainer::ConfigureBindings()
     // and Y is defined as to the left according to WPILib convention.
     drivetrain.SetDefaultCommand(
         // Drivetrain will execute this command periodically
-        // If joystick is idle, set X formation
-        // Else: drive normally
-        frc2::cmd::Run([this] { drivetrain.DriveDefaultCommand(
-            -joystick.GetLeftY() * MaxSpeed, // Drive forward with negative Y (forward)
-            -joystick.GetLeftX() * MaxSpeed, // Drive left with negative X (left)
-            -joystick.GetRightX() * MaxAngularRate, // Drive counterclockwise with negative X (left)
-            drive);})
+        drivetrain.ApplyRequest([this]() -> auto&& {
+            return drive.WithVelocityX(-joystick.GetLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                .WithVelocityY(-joystick.GetLeftX() * MaxSpeed) // Drive left with negative X (left)
+                .WithRotationalRate(-joystick.GetRightX() * MaxAngularRate); // Drive counterclockwise with negative X (left)
+        })
     );
 
     // Idle while the robot is disabled. This ensures the configured
@@ -35,17 +33,14 @@ void RobotContainer::ConfigureBindings()
         }).IgnoringDisable(true)
     );
 
-    // Intake controls
-
-    joystick.A().WhileTrue(frc2::cmd::Run([this] { intake.RunIntake(1.0); }))
-        .OnFalse(frc2::cmd::Run([this] { intake.RunIntake(0.0); }));
-    joystick.B().WhileTrue(frc2::cmd::Run([this] { intake.RunIntake(-1.0); }))
-        .OnFalse(frc2::cmd::Run([this] { intake.RunIntake(0.0); }));
-    joystick.X().OnTrue(frc2::cmd::Run([this] { intake.SetIntakePosition(false); }));
-    joystick.Y().OnTrue(frc2::cmd::Run([this] { intake.SetIntakePosition(true); }));
+    joystick.A().WhileTrue(drivetrain.ApplyRequest([this]() -> auto&& { return brake; }));
+    joystick.B().WhileTrue(drivetrain.ApplyRequest([this]() -> auto&& {
+        return point.WithModuleDirection(frc::Rotation2d{-joystick.GetLeftY(), -joystick.GetLeftX()});
+    }));
 
     // Shooter controls
 
+    /*
     joystick.LeftTrigger().WhileTrue(frc2::cmd::Run([this] {
             double speed = joystick.GetLeftTriggerAxis();
             shooter.SpinUpShooter(speed);
@@ -56,54 +51,6 @@ void RobotContainer::ConfigureBindings()
     joystick.RightTrigger().WhileTrue(frc2::cmd::Run([this]
         { shooter.SetIndexerSpeed(joystick.GetRightTriggerAxis()); }))
         .WhileFalse(frc2::cmd::Run([this] { shooter.SetIndexerSpeed(0.0); }));
-
-    // Climber controls
-
-    joystick.LeftBumper().WhileTrue(frc2::cmd::Run([this] { climber.SetClimberPosition(-1.0); }))
-        .OnFalse(frc2::cmd::Run([this] { climber.SetClimberPosition(0.0); }));
-    joystick.RightBumper().WhileTrue(frc2::cmd::Run([this] { climber.SetClimberPosition(1.0); }))
-        .OnFalse(frc2::cmd::Run([this] { climber.SetClimberPosition(0.0); }));
-
-    // Auto-align controls
-
-    // Align with hub (target) and power up shooter to appropriate speed
-    /*
-    joystick.POVUp().WhileTrue(
-        frc2::cmd::Sequence(
-            drivetrain.ApplyRequest([this]() -> auto&& {
-                return drive.WithVelocityX(0_mps) // Drive forward with negative Y (forward)
-                    .WithVelocityY(0_mps) // Drive left with negative X (left)
-                    .WithRotationalRate(units::angular_velocity::radians_per_second_t{0}); // Drive counterclockwise with negative X (left)
-            }),
-            frc2::cmd::Run([this] { shooter.SpinUpShooter(true); })
-        )
-    )
-    .OnFalse(
-        frc2::cmd::Sequence(
-            // Get vision target data
-            frc2::cmd::Run([this] { vision.setVisionTarget(hubVisionTarget); }),
-            // Set drivetrain angle - TODO: Implement actual alignment logic
-            frc2::cmd::Run([this] { drivetrain.DriveDefaultCommand(0_mps, 0_mps, 0_rad_per_s, drive); }),
-            // Set shooter speed
-            frc2::cmd::Run([this] { shooter.SpinUpShooter(shooter.getTargetShooterRPM(hubVisionTarget.range)); })
-        )
-    );
-
-    // Align with tower: Snap robot angle to 90 degrees or -90 degrees
-    joystick.POVDown().WhileTrue(
-        drivetrain.ApplyRequest([this]() -> auto&& {
-                return drive.WithVelocityX(0_mps) // Drive forward with negative Y (forward)
-                    .WithVelocityY(0_mps) // Drive left with negative X (left)
-                    .WithRotationalRate(units::angular_velocity::radians_per_second_t{0}); // Drive counterclockwise with negative X (left)
-            })
-    )
-    .OnFalse(
-        drivetrain.ApplyRequest([this]() -> auto&& {
-            return drive.WithVelocityX(0_mps) // Drive forward with negative Y (forward)
-                .WithVelocityY(0_mps) // Drive left with negative X (left)
-                .WithRotationalRate(units::angular_velocity::radians_per_second_t{0}); // Drive counterclockwise with negative X (left)
-        })
-    );
     */
 
     // Run SysId routines when holding back/start and X/Y.
@@ -113,7 +60,11 @@ void RobotContainer::ConfigureBindings()
     (joystick.Start() && joystick.Y()).WhileTrue(drivetrain.SysIdQuasistatic(frc2::sysid::Direction::kForward));
     (joystick.Start() && joystick.X()).WhileTrue(drivetrain.SysIdQuasistatic(frc2::sysid::Direction::kReverse));
 
-    }
+    // reset the field-centric heading on left bumper press
+    joystick.LeftBumper().OnTrue(drivetrain.RunOnce([this] { drivetrain.SeedFieldCentric(); }));
+
+    drivetrain.RegisterTelemetry([this](auto const &state) { logger.Telemeterize(state); });
+}
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand()
 {
